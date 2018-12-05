43312bb8c3891539678c32ef8da70b9f2b496d86
Susanna <sfalan@utu.fi>
2018-12-05 15:00:35 +0200

    added play again question

--- game.py
+++ game.py
@@ -1,17 +1,5 @@
 from random import randint
 
-winner = False
-moves = 0
-winCombinations = [
-    (0, 1, 2),
-    (3, 4, 5),
-    (6, 7, 8),
-    (0, 3, 6),
-    (1, 4, 7),
-    (2, 5, 8),
-    (0, 4, 8),
-    (2, 4, 6)]
-
 def initializeBoard():
     return [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
@@ -29,6 +17,7 @@ def initializeSettings():
 
     return (playMode, players, currentPlayer)
 
+
 def draw_board(board):
     print board[6], " ", board[7], " ", board[8]
     print board[3], " ", board[4], " ", board[5]
@@ -44,9 +33,9 @@ def is_legal_move(board, move):
 
 
 def ask_for_move(board):
-    move = input("Click the number where you want to place your marker: ")
+    move = input("Give the number where you want to place your marker: ")
     while not (is_legal_move(board, move)):
-        move = input("Invalid move! Click the number where you want to place your marker: ")
+        move = input("Invalid move! Give the number where you want to place your marker: ")
     return move
 
 
@@ -93,37 +82,29 @@ def check_winner(board, currentPlayer):
     return currentPlayerHasWon
 
 
-board = initializeBoard()
-settings = initializeSettings()
-playMode = settings[0]
-players = settings[1]
-currentPlayer = settings[2]
+while(raw_input("Do you want to play? (y/n)") == "y"):
+    winner = False
+    moves = 0
+    winCombinations = [
+        (0, 1, 2),
+        (3, 4, 5),
+        (6, 7, 8),
+        (0, 3, 6),
+        (1, 4, 7),
+        (2, 5, 8),
+        (0, 4, 8),
+        (2, 4, 6)]
 
-while not winner:
+    board = initializeBoard()
+    settings = initializeSettings()
+    playMode = settings[0]
+    players = settings[1]
+    currentPlayer = settings[2]
 
-    if playMode == 1:
+    while not winner:
 
-        print currentPlayer[0] + " in turn" + "\n"
-        move = ask_for_move(board)
-        board[move - 1] = currentPlayer[1]
-        moves += 1
-        draw_board(board)
-        winner = check_winner(board, currentPlayer)
-        if moves == 9:
-            print "No winner this time."
-            winner = True
-            break
-        if winner:
-            print currentPlayer[0] + " wins!"
-            break
-        if currentPlayer[0] == "Player 1":
-            currentPlayer = players["player2"]
-        else:
-            currentPlayer = players["player1"]
+        if playMode == 1:
 
-    if playMode == 2:
-
-        if currentPlayer == players["player"]:
             print currentPlayer[0] + " in turn" + "\n"
             move = ask_for_move(board)
             board[move - 1] = currentPlayer[1]
@@ -137,35 +118,47 @@ while not winner:
             if winner:
                 print currentPlayer[0] + " wins!"
                 break
-            if currentPlayer[0] == "Player":
-                currentPlayer = players["computer"]
+            if currentPlayer[0] == "Player 1":
+                currentPlayer = players["player2"]
             else:
-                currentPlayer = players["player"]
+                currentPlayer = players["player1"]
 
-        if currentPlayer == players["computer"]:
-            print currentPlayer[0] + " in turn" + "\n"
-            move = computers_move(board)
-            board[move - 1] = currentPlayer[1]
-            moves += 1
-            draw_board(board)
-            winner = check_winner(board, currentPlayer)
-            if moves == 9:
-                print "No winner this time."
-                winner = True
-                break
-            if winner:
-                print currentPlayer[0] + " wins!"
-                break
-            if currentPlayer[0] == "Player":
-                currentPlayer = players["computer"]
-            else:
-                currentPlayer = players["player"]
+        if playMode == 2:
 
-    #if winner:
-        #continueGame = raw_input("Do you want to play again? (y/n)")
-        #if continueGame == "y":
-            #board = initializeBoard()
-            #settings = initializeSettings()
-            #playMode = settings[0]
-            #players = settings[1]
-            #currentPlayer = settings[2]
+            if currentPlayer == players["player"]:
+                print currentPlayer[0] + " in turn" + "\n"
+                move = ask_for_move(board)
+                board[move - 1] = currentPlayer[1]
+                moves += 1
+                draw_board(board)
+                winner = check_winner(board, currentPlayer)
+                if moves == 9:
+                    print "No winner this time."
+                    winner = True
+                    break
+                if winner:
+                    print currentPlayer[0] + " wins!"
+                    break
+                if currentPlayer[0] == "Player":
+                    currentPlayer = players["computer"]
+                else:
+                    currentPlayer = players["player"]
+
+            if currentPlayer == players["computer"]:
+                print currentPlayer[0] + " in turn" + "\n"
+                move = computers_move(board)
+                board[move - 1] = currentPlayer[1]
+                moves += 1
+                draw_board(board)
+                winner = check_winner(board, currentPlayer)
+                if moves == 9:
+                    print "No winner this time."
+                    winner = True
+                    break
+                if winner:
+                    print currentPlayer[0] + " wins!"
+                    break
+                if currentPlayer[0] == "Player":
+                    currentPlayer = players["computer"]
+                else:
+                    currentPlayer = players["player"]
