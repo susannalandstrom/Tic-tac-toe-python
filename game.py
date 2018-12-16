2f2f864afe5c5fdf4ad59985c8e56856bcd5bcfa
nawkah <45449190+nawkah@users.noreply.github.com>
2018-12-16 18:57:33 +0200

    Update game.py

--- game.py
+++ game.py
@@ -9,7 +9,7 @@ def initializeSettings():
     print ("Play modes:" + "\n" + "1. 2 Players" + "\n" + "2. Against computer" + "\n")
     playMode = raw_input("Choose play mode: " + "\n")
 
-    if is_legal_play_mode(playMode):
+    if isLegalPlayMode(playMode):
         playMode = int(playMode)
 
         if playMode == 1:
@@ -28,7 +28,7 @@ def initializeSettings():
 
 
 #prints the board
-def draw_board(board):
+def drawBoard(board):
     print board[6], " ", board[7], " ", board[8]
     print board[3], " ", board[4], " ", board[5]
     print board[0], " ", board[1], " ", board[2]
@@ -36,14 +36,14 @@ def draw_board(board):
 
 
 #defines whether the play mode input is legal
-def is_legal_play_mode(mode):
+def isLegalPlayMode(mode):
     if mode == "1" or mode == "2":
         return True
     return False
 
 
-#defines whether the move is legal, meaning that it is a number from 1 to 9 and the place is still empty on the board
-def is_legal_move(board, move):
+#defines whether the move is legal, meaning that it is a number from 1 to 9 and that the place on the board is still empty
+def isLegalMove(board, move):
     if isinstance(move, int) or move.isdigit():
         if 0 < int(move) < 10:
             if isinstance(board[int(move) - 1], int):
@@ -51,18 +51,18 @@ def is_legal_move(board, move):
     return False
 
 
-#function is called when player is on turn
+#function is called when it is player's turn
 #asks the user for input and checks whether the move is legal
-def ask_for_move(board):
-    move = raw_input("Give the number where you want to place your marker: ")
-    while not (is_legal_move(board, move)):
-        move = raw_input("Invalid move! Give the number where you want to place your marker: ")
+def askForMove(board):
+    move = raw_input("Give the number where you want to place your mark: ")
+    while not (isLegalMove(board, move)):
+        move = raw_input("Invalid move! Give the number where you want to place your mark: ")
     return int(move)
 
 
-#function is called when computer is on turn
-def computers_move(board):
-    #checks if computer can win with this move (has already to marks aligned)
+#function is called when it is computer's turn
+def computersMove(board):
+    #checks if computer can win with this move (has already two marks aligned)
     for combination in winCombinations:
         count = 0
         for i in combination:
@@ -73,7 +73,7 @@ def computers_move(board):
                         if isinstance(board[j], int):
                             return j+1
 
-    #checks if player has to marks aligned and places his mark on the third blocking the player
+    #checks if player has two marks aligned and places his mark on the third one blocking the player
     for combination in winCombinations:
         count = 0
         for i in combination:
@@ -84,19 +84,19 @@ def computers_move(board):
                         if isinstance(board[j], int):
                             return j+1
 
-    #places the mark in the middle of the board if possible, if not, mark is randomly placed to a free spot
-    if is_legal_move(board, 5):
+    #places the mark in the middle of the board if possible, if not, mark is randomly placed in a free spot
+    if isLegalMove(board, 5):
         return 5
     else:
         move = randint(0, 8)
-        while not (is_legal_move(board, move)):
+        while not (isLegalMove(board, move)):
             move = randint(0, 8)
         return move+1
 
 
 #winner is checked on each turn
 #if the currentPlayer's mark is found in a combination similar to winning combination, currentPlayer wins
-def check_winner(board, currentPlayer):
+def checkWinner(board, currentPlayer):
     currentPlayerHasWon = False
     for combination in winCombinations:
         count = 0
@@ -109,7 +109,7 @@ def check_winner(board, currentPlayer):
     return currentPlayerHasWon
 
 
-#loop while the player chooses to continue playing
+#loop while the player chooses whether to continue playing or not
 while(raw_input("Do you want to play? (y/n)") == "y"):
     winner = False
     moves = 0
@@ -128,7 +128,7 @@ while(raw_input("Do you want to play? (y/n)") == "y"):
     playMode = settings[0]
     players = settings[1]
     currentPlayer = settings[2]
-    draw_board(board)
+    drawBoard(board)
 
     #loop while there is no winner
     while not winner:
@@ -137,11 +137,11 @@ while(raw_input("Do you want to play? (y/n)") == "y"):
         if playMode == 1:
 
             print currentPlayer[0] + " in turn" + "\n"
-            move = ask_for_move(board)
+            move = askForMove(board)
             board[move - 1] = currentPlayer[1]                  #players move is placed on the board
             moves += 1
-            draw_board(board)
-            winner = check_winner(board, currentPlayer)         #possible winner is checked on each turn
+            drawBoard(board)
+            winner = checkWinner(board, currentPlayer)         #possible winner is checked on each turn
             if winner:
                 print currentPlayer[0] + " wins!"
                 break
@@ -161,11 +161,11 @@ while(raw_input("Do you want to play? (y/n)") == "y"):
             #when player is on turn
             if currentPlayer == players["player"]:
                 print currentPlayer[0] + " in turn" + "\n"
-                move = ask_for_move(board)
+                move = askForMove(board)
                 board[move - 1] = currentPlayer[1]
                 moves += 1
-                draw_board(board)
-                winner = check_winner(board, currentPlayer)
+                drawBoard(board)
+                winner = checkWinner(board, currentPlayer)
                 if winner:
                     print currentPlayer[0] + " wins!"
                     break
@@ -181,11 +181,11 @@ while(raw_input("Do you want to play? (y/n)") == "y"):
             #when computer is on turn
             if currentPlayer == players["computer"]:
                 print currentPlayer[0] + " in turn" + "\n"
-                move = computers_move(board)
+                move = computersMove(board)
                 board[move - 1] = currentPlayer[1]              #computers playmark is added to the board
                 moves += 1
-                draw_board(board)
-                winner = check_winner(board, currentPlayer)
+                drawBoard(board)
+                winner = checkWinner(board, currentPlayer)
                 if winner:
                     print currentPlayer[0] + " wins!"
                     break
