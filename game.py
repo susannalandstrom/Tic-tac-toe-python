6e3eef6a234cc8a5142ca71f254f025550abc5dd
nawkah <45449190+nawkah@users.noreply.github.com>
2018-12-17 14:16:14 +0200

    Update game.py

--- game.py
+++ game.py
@@ -29,9 +29,9 @@ def initializeSettings():
 
 #prints the board
 def drawBoard(board):
-    print board[6], " ", board[7], " ", board[8]
-    print board[3], " ", board[4], " ", board[5]
     print board[0], " ", board[1], " ", board[2]
+    print board[3], " ", board[4], " ", board[5]
+    print board[6], " ", board[7], " ", board[8]
     print ""
 
 
